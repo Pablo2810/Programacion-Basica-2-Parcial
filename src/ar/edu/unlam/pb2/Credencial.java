e5abbb14ecc3eb34be592605271766e057a58f21
Pablo2810 <pablorocha.2810@gmail.com>
2023-05-10 10:40:00 -0300

    Test basicos y clases Empresa, Credencial

---
+++ src/ar/edu/unlam/pb2/Credencial.java
@@ -0,0 +1,19 @@
+package ar.edu.unlam.pb2;
+
+public class Credencial {
+	
+	private Integer id;
+	
+	public Credencial(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/ar/edu/unlam/pb2/Empresa.java
@@ -0,0 +1,19 @@
+package ar.edu.unlam.pb2;
+
+public class Empresa {
+
+	private String nombre;
+	
+	public Empresa(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
